f91bc68fff1a5ef50fb667026297dab06fb4036b
Bastin Jerry <bastin@nextaso.com>
2023-08-09 00:21:27 +0530

    asa

--- prs_project/settings.py
+++ prs_project/settings.py
@@ -69,10 +69,14 @@ WSGI_APPLICATION = 'prs_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviegeek',                      
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 # DATABASES = {
 #     'default': {
